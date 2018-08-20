2004e09c13c11cff2f8bd5d8a7092fe939bd440c
Liam Huang <liamhuang0205@gmail.com>
2018-08-21 00:03:03 +0800

    169, prettify.

--- algorithm/169-majority-element/solution.cc
+++ algorithm/169-majority-element/solution.cc
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(std::vector<int>& nums) {
         size_t len = nums.size();
         size_t limit = floor(len / 2);
-        map<int, size_t> counter;
+        std::unordered_map<int, size_t> counter;
         for (size_t i = 0; i != len; ++i) {
-            if (counter.count(nums[i])) // we've test nums[i], and it's not the answer, so pass
+            const int work = nums[i];
+            if (counter.find(work) != counter.end()) {
                 continue;
-            else // test num[i], count the number of its appearance in the vector
-                // nums.begin() + i, this is easy to understand and could boost the speed
-                counter[nums[i]] = count((nums.begin() + i), nums.end(), nums[i]);
-            if (counter[nums[i]] > limit)
-                // if counter[nums[i]] > limit, return nums[i] immediately
-                return nums[i];
+            } else {
+                counter.insert({work, std::count((nums.begin() + i), nums.end(), work)});
+            }
+            if (counter[work] > limit) {
+                return work;
+            }
         }
     }
 };
