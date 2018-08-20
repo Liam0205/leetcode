78ddb5e8f70e783cf252577d1db2231376368b2b
Liam Huang <liamhuang0205@gmail.com>
2018-08-20 23:28:07 +0800

    022, modified.

--- algorithm/022-generate-parentheses/solution.cc
+++ algorithm/022-generate-parentheses/solution.cc
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
-        vector<string> ret;
-        helper(ret, "", n, n);
-        return ret;
+        std::vector<std::string> res;
+        recursive_search(n, n, "", &res);
+        return res;
     }
-    void helper(vector<string> & res, string str, int left, int right){
-        if(left == 0 && right == 0){
-            res.push_back(str);
-            return;
+
+private:
+    void recursive_search(int left, int right, std::string generated, std::vector<std::string>* res) {
+        if (left == 0 and right == 0) {
+            res->push_back(generated);
+        } else {
+            if (right > 0 and left < right) {
+                recursive_search(left, right - 1, generated + ")", res);
+            }
+            if (left > 0) {
+                recursive_search(left - 1, right, generated + "(", res);
+            }
         }
-        if(left > 0) helper(res, str + "(", left - 1, right);
-        if(right > left) helper(res, str + ")", left, right - 1);
     }
 };
