9d172ee32149464d24265a333172be84af657df4
Liam Huang <liamhuang0205@gmail.com>
2018-08-22 11:11:19 +0800

    242, prettify.

--- algorithm/242-valid-anagram/solution.cc
+++ algorithm/242-valid-anagram/solution.cc
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        vector<int> vec_int_counter(26, 0);
-        size_t sz_length = s.length();
-        for (size_t i = 0; i != sz_length; ++i) {
-            ++vec_int_counter.at(s.at(i) - 'a');
-            --vec_int_counter.at(t.at(i) - 'a');
+    bool isAnagram(const std::string& s, const std::string& t) {
+        const size_t ssz = s.size();
+        const size_t tsz = t.size();
+        if (ssz != tsz) {
+            return false;
         }
-        for (size_t i = 0; i != 26; ++i) {
-            if (vec_int_counter.at(i) != 0) return false;
+        std::vector<int> counter(26, 0);
+        for (size_t i = 0; i != ssz; ++i) {
+            ++counter[s[i] - 'a'];
+            --counter[t[i] - 'a'];
         }
-        return true;
+        return std::all_of(counter.cbegin(), counter.cend(), [](auto i) { return i == 0; });
     }
 };
+
