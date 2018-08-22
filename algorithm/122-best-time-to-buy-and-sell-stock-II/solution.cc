3c845ace792026db68c8a02d1e03da642bdf54ff
Liam Huang <liamhuang0205@gmail.com>
2018-08-22 10:50:04 +0800

    122, update.

--- algorithm/122-best-time-to-buy-and-sell-stock-II/solution.cc
+++ algorithm/122-best-time-to-buy-and-sell-stock-II/solution.cc
@@ -1,11 +1,15 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        if (prices.size() < 2) return 0;
-        size_t sz_length = prices.size();
-        int int_sum = 0;
-        for (size_t i = 1; i != sz_length; ++i)
-            int_sum += max (0, prices.at(i) - prices.at(i - 1));
-        return int_sum;
+    int maxProfit(std::vector<int>& prices) {
+        const size_t sz = prices.size();
+        if (sz < 2) {
+            return 0;
+        }
+        int res = 0;
+        for (size_t i = 1; i != sz; ++i) {
+            res += std::max(0, prices[i] - prices[i - 1]);
+        }
+        return res;
     }
 };
+
