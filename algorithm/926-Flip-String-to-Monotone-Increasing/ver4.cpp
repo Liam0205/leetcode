b9e27ef728b30810c96b0d775a1fbd173b29b3f6
Liam Huang <Liam0205@users.noreply.github.com>
2018-11-05 11:21:58 +0800

    Update ver4.cpp

--- algorithm/926-Flip-String-to-Monotone-Increasing/ver4.cpp
+++ algorithm/926-Flip-String-to-Monotone-Increasing/ver4.cpp
@@ -1,4 +1,4 @@
-static int x= [](){ std::ios::sync_with_stdio(0); cin.tie(0); return 0;}();
+static int x= [](){ std::ios::sync_with_stdio(0); cin.tie(0); return 0; }();
 class Solution {
 public:
     int minFlipsMonoIncr(string S) {
