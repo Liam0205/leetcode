33850fde042d83e983432f39503cbd2ee5a43e1d
Liam Huang <liamhuang0205@gmail.com>
2019-02-28 22:07:53 +0800

    [031] done.

---
+++ algorithm/031-next-permutation/ver1.cc
@@ -0,0 +1,13 @@
+static const auto io_sync_off = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
+}();
+
+class Solution {
+public:
+  void nextPermutation(std::vector<int>& nums) {
+    std::next_permutation(nums.begin(), nums.end());
+  }
+};
