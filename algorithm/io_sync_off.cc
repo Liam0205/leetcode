e865c00e59b76aebf3fed99b83f220dd38c3b4ba
Liam Huang <liamhuang0205@gmail.com>
2019-02-28 14:34:54 +0800

    [io_sync_off] indent length: 2.

--- algorithm/io_sync_off.cc
+++ algorithm/io_sync_off.cc
@@ -1,7 +1,7 @@
 static const auto io_sync_off = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
 }();
 
