4f07c83c822f9165124c09640dcd6ee198d19f25
Liam Huang <liamhuang0205@gmail.com>
2019-08-22 15:56:02 +0800

    [0322] ver1, done.

---
+++ algorithm/0322-Coin-Changes/ver1.cc
@@ -0,0 +1,27 @@
+static const auto io_sync_off = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  return nullptr;
+} ();
+
+class Solution {
+ public:
+  int coinChange(vector<int>& coins, int amount) {
+    std::vector<int> dp(amount + 1, googol);
+    dp[0] = 0;
+
+    for (const int& coin : coins) {
+      for (int i = coin; i < amount + 1; ++i) {
+        if (dp[i - coin] != googol) {
+          dp[i] = std::min(dp[i], dp[i - coin] + 1);
+        }
+      }
+    }
+
+    return (dp[amount] == googol) ? -1 : dp[amount];
+  }
+
+ private:
+  static const constexpr int googol = std::numeric_limits<int>::max();  // LOL
+};
+
