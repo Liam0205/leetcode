d3dbad49d3a21ce2541701b0eeb287051dcacf9d
Liam Huang <liamhuang0205@gmail.com>
2019-08-23 21:49:23 +0800

    [0175] ver1, done. optimized.

---
+++ database/0175-Combine-Two-Tables/ver1.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+  Person.FirstName,
+  Person.LastName,
+  Address.City,
+  Address.State
+FROM
+  Person LEFT JOIN Address ON
+    Person.PersonId = Address.PersonId;
