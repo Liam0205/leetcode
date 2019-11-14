9c4dcbe5715e7dcb1f9f6f853c51914c5827f400
Liam Huang <liamhuang0205@gmail.com>
2019-11-14 18:54:58 +0800

    [1170] ver2, done. optimized.

---
+++ algorithm/1170-compare-strings-by-frequency-of-the-smallest-character/ver2.cc
@@ -0,0 +1,49 @@
+#pragma GCC optimise ("Ofast")
+
+static const auto io_sync_off = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
+}();
+
+class Solution {
+ public:
+  std::vector<int> numSmallerByFrequency(const std::vector<std::string>& queries,
+                                         const std::vector<std::string>& words) {
+    size_t recoder[12];  // max_len == 10, plus one for zero-based indexing, plus one for line #29
+    std::fill(std::begin(recoder), std::end(recoder), 0);
+    for (const std::string& word : words) {
+      ++recoder[freqfunc(word)];
+    }
+    size_t bigger_sum = 0;
+    std::transform(std::rbegin(recoder), std::rend(recoder), std::rbegin(recoder),
+        [&](size_t count) {
+          bigger_sum += count;
+          return bigger_sum;
+        });
+
+    std::vector<int> res;
+    res.reserve(queries.size());
+    for (const std::string& query : queries) {
+      res.push_back(recoder[freqfunc(query) + 1]);
+    }
+    return res;
+  }
+
+ private:
+  size_t freqfunc(const std::string& s) {
+    size_t res = 0;
+    char   minc = s[0];
+    for (auto c : s) {
+      if (c == minc) {
+        ++res;
+      } else if (c < minc) {
+        minc = c;
+        res = 1;
+      } else /* ignored */;
+    }
+    return res;
+  }
+};
+
