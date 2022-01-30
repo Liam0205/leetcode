f15c271f97fe844cb0b232b54593d76f886a20e0
Liam Huang <liamhuang0205@gmail.com>
2022-01-30 12:34:53 +0800

    [0047] add ver1.

---
+++ algorithm/0047-permutations-ii/ver1.cc
@@ -0,0 +1,18 @@
+static const auto io_sync_off = []() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
+  return nullptr;
+}();
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    std::vector<std::vector<int>> res;
+    do {
+      res.emplace_back(nums);
+    } while (std::next_permutation(nums.begin(), nums.end()));
+    return res;
+  }
+};
